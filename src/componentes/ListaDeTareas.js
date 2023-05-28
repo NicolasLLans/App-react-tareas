3ff1a49f3dd29d67abfcabc95fdd27490b8f66f2
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-05-28 01:38:21 -0300

    funcionalidad eliminar tareas, terminada

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -15,6 +15,11 @@ function ListaDeTareas() {
     }
   }
 
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !=id);
+    setTareas(tareasActualizadas);
+  }
+
 
   return (
     <>
@@ -24,8 +29,11 @@ function ListaDeTareas() {
         {
           tareas.map((tarea) =>
           <Tarea
+          key={tarea.id}
+          id={tarea.id}
           texto={tarea.texto}
-          completada={tarea.completada} 
+          completada={tarea.completada}
+          eliminarTarea={eliminarTarea} 
           />
           )
         }
