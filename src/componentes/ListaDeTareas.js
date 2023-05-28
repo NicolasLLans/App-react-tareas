1e9bf560d6ca3d97daa05c24845f13843d7a077c
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-05-28 02:05:53 -0300

    funcionalidad completar tarea lista

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -13,13 +13,22 @@ function ListaDeTareas() {
       const tareasActualizadas = [tarea, ...tareas];
       setTareas(tareasActualizadas)
     }
-  }
+  };
 
   const eliminarTarea = id => {
     const tareasActualizadas = tareas.filter(tarea => tarea.id !=id);
     setTareas(tareasActualizadas);
-  }
+  };
 
+  const completarTarea = id => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if(tarea.id === id){
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  };
 
   return (
     <>
@@ -34,6 +43,7 @@ function ListaDeTareas() {
           texto={tarea.texto}
           completada={tarea.completada}
           eliminarTarea={eliminarTarea} 
+          completarTarea={completarTarea}
           />
           )
         }
