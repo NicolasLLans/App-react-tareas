dad9c3ce56987fa90ee1a026cb3300992d6caffd
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-05-28 01:12:26 -0300

    funcionalidad agregar tareas, terminada

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -2,15 +2,24 @@ import React, { useState } from 'react';
 import TareaFormulario from './TareaFormulario';
 import '../hojas-de-estilos/ListaDeTareas.css';
 import Tarea from './Tarea.js'
+
 function ListaDeTareas() {
 
   const [tareas, setTareas] = useState([]);
 
+  const agregarTarea = tarea => {
+    if(tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas)
+    }
+  }
 
 
   return (
     <>
-      <TareaFormulario />
+      <TareaFormulario 
+      onSubmit={agregarTarea} />
       <div className='tareas-lista-contenedor'>
         {
           tareas.map((tarea) =>
--- src/componentes/TareaFormulario.js
+++ src/componentes/TareaFormulario.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../hojas-de-estilos/TareaFormulario.css'
+import { v4 as uuid } from 'uuid'
+
 function TareaFormulario(props) {
+
+  const [input, setInput]=useState('');
+
+  const manejarCambio = e => {
+    setInput(e.target.value)
+  }
+
+  const manejarEnvio = e => {
+    e.preventDefault();
+
+    const tareaNueva = {
+      id: uuid(),
+      texto: input,
+      completado: false
+    }
+
+    props.onSubmit(tareaNueva);
+  }
+
   return(
-    <form className='tarea-formulario'>
+    <form 
+      className='tarea-formulario'
+      onSubmit={manejarEnvio} >
       <input
         className='tarea-input'
         type='text'
         placeholder='Escribe una Tarea'
         name='texto'
+        onChange={manejarCambio}
          />
          <button className='tarea-boton'>
             Agregar Tarea
